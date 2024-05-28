525c807277138e078794e62452c8cd3cf32aa775
Davs07 <botsymeisterspieler@gmail.com>
2024-05-28 03:00:14 -0500

    completed dropdown menu of UserButton

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -3,6 +3,27 @@ import { getUser } from "@/hooks/useUser";
 import { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 
+import {
+  Activity,
+  CircleHelp,
+  Keyboard,
+  LogOut,
+  Settings,
+  Star,
+  User2,
+} from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 function UserButton() {
   const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
@@ -32,14 +53,65 @@ function UserButton() {
   }
 
   return (
-    <Button variant={"none"} className="rounded-xl">
-      <img
-        src={user.avatar_url}
-        alt={user.login}
-        className="w-8 h-8 rounded-full"
-      />
-      <span className="ml-2">{user.login}</span>
-    </Button>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant={"none"} className="rounded-xl">
+            <img
+              src={user.avatar_url}
+              alt={user.login}
+              className="w-8 h-8 rounded-full"
+            />
+            <span className="ml-2">{user.login}</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56 z-[99999999] ">
+          <DropdownMenuLabel>{user.login}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <User2 className="mr-2 h-4 w-4" />
+              <span>Mi perfil</span>
+              <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className="mr-2 h-4 w-4" />
+              <span>Configuración</span>
+              <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <CircleHelp className="mr-2 h-4 w-4" />
+              <span>Centro de ayuda</span>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <Keyboard className="mr-2 h-4 w-4" />
+              <span>Atajos de teclado</span>
+              <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Activity className="mr-2 h-4 w-4" />
+              <span>Registro de actividad</span>
+              <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem>
+            <Star className="mr-2 h-4 w-4" />
+            <span>Actualizar a Premium</span>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <LogOut className="mr-2 h-4 w-4" />
+            <span>Cerrar sesión</span>
+            <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 }
 
