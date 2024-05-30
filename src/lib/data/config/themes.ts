1af6c2fa37881b74e78f3cda116cf6e2428a8add
Davs07 <botsymeisterspieler@gmail.com>
2024-05-30 12:09:15 -0500

    Updated theme configuration and theme switcher component

--- src/lib/data/config/themes.ts
+++ src/lib/data/config/themes.ts
@@ -2,13 +2,46 @@ export type Theme = {
   id: string;
   name: string;
   main: string;
+  bg: string;
 };
 
 const themes: Theme[] = [
-  { id: "theme-1", name: "Blue Orchid", main: "233° 100% 57%" },
-  { id: "theme-2", name: "Poison Green", main: "113 100% 57%" },
-  { id: "theme-3", name: "Yellowy Green", main: "72 91% 55%" },
-  { id: "theme-4", name: "French Rose", main: "342 97% 62%" },
+  {
+    id: "theme-1",
+    name: "Theme 1",
+    main: "233 100% 57%",
+    bg: "bg-[hsl(233 100% 57%)]",
+  },
+  {
+    id: "theme-2",
+    name: "Theme 2",
+    main: "113 100% 57%",
+    bg: "bg-[hsl(113 100% 57%)]",
+  },
+  {
+    id: "theme-3",
+    name: "Theme 3",
+    main: "72 91% 55%",
+    bg: "bg-[hsl(72 91% 55%)]",
+  },
+  {
+    id: "theme-4",
+    name: "Theme 4",
+    main: "342 97% 62%",
+    bg: "bg-[hsl(342 97% 62%)]",
+  },
 ];
 
 export default themes;
+
+/*   { id: "theme-1", name: "Blue Orchid", main: "233° 100% 57%";
+    bg: "bg-[var(hsl(233° 100% 57%))]" }
+   },
+  { 
+    id: "theme-2"; 
+    name: "Poison Green"; 
+    main: "113 100% 57%";
+    bg: "bg-[var(hsl(113 100% 57%))]"
+   },
+  { id: "theme-3", name: "Yellowy Green", main: "72 91% 55%" },
+  { id: "theme-4", name: "French Rose", main: "342 97% 62%" }, */
--- src/sections/Navbar/ThemeSwitcher.tsx
+++ src/sections/Navbar/ThemeSwitcher.tsx
@@ -1,31 +1,51 @@
 import React from "react";
 import useThemeStore from "@/hooks/useThemeStore"; // Ajusta la ruta según tu estructura de proyecto
+import themes, { Theme } from "@/lib/data/config/themes";
 
 const ThemeSwitcher = () => {
-  const { theme, isDarkMode, themes, toggleDarkMode, changeTheme } =
-    useThemeStore();
+  const { theme, isDarkMode, toggleDarkMode, changeTheme } = useThemeStore();
 
   return (
     <div className="min-h-max bg-background text-foreground flex flex-col gap-6">
       <div className="flex flex-col gap-3">
         <h5>Modo</h5>
-        <button
-          onClick={toggleDarkMode}
-          className="m-4 p-2 bg-primary-foreground text-primary rounded">
-          Toggle Dark/Light
-        </button>
+
+        <label
+          className="cursor-pointer relative h-[3em] w-[6em] rounded-full bg-card "
+          onChange={toggleDarkMode}>
+          <span className="absolute inset-[0.1em] rounded-full border-[1px] border-primary"></span>
+          <div className="absolute left-[0.5em] top-1/2 flex h-[2em] w-[2em] -translate-y-1/2 items-center justify-center rounded-full bg-card shadow-[inset_0px_2px_2px_0px_hsl(0,0%,85%)]">
+            <div className="h-[1.5em] w-[1.5em] rounded-full bg-secondary-foreground shadow-[0px_2px_2px_0px_hsl(0,0%,85%)]"></div>
+          </div>
+          <div className="absolute right-[0.5em] top-1/2 h-[0.25em] w-[1.5em] -translate-y-1/2 rounded-full bg-input shadow-[inset_0px_2px_1px_0px_hsl(0,0%,40%)]"></div>
+          <input
+            className="peer h-[1em] w-[1em] opacity-0"
+            id=""
+            name=""
+            type="checkbox"
+          />
+          <span className="absolute left-[0.25em] top-1/2 flex h-[2.5em] w-[2.5em] -translate-y-1/2 items-center justify-center rounded-full bg-primary shadow-[inset_4px_4px_4px_0px_rgba(64,64,64,0.25),inset_-4px_-4px_4px_0px_rgba(16,16,16,0.5)] duration-300 peer-checked:left-[calc(100%-2.75em)]">
+            <span className="relative h-full w-full rounded-full">
+              <span className="absolute inset-[0.1em] rounded-full border border-input"></span>
+            </span>
+          </span>
+        </label>
       </div>
       <div className="flex flex-col gap-3">
         <h5>Tema</h5>
-        <div className="grid-2">
+        <div className="grid grid-cols-2 gap-4 w-full max-w-[540px]">
           {themes.map((t) => (
-            <button
+            <div
               key={t.id}
               onClick={() => changeTheme(t.id)}
-              className={`m-4 p-2 rounded bg-main text-white`}
-              style={{ backgroundColor: `hsl(${t.main})` }}>
-              {t.name}
-            </button>
+              className="w-full h-max border-4 rounded-lg border-input bg-background">
+              <div className="w-12 h-8 bg-card"></div>
+              <button
+                className={`m-4 p-2 rounded text-primary`}
+                style={{ backgroundColor: `hsl(${t.main})` }}>
+                {t.name}
+              </button>
+            </div>
           ))}
         </div>
       </div>
